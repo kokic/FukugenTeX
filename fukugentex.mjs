5fc169c322ce2e43961a84c2ccab4242788d6ffb
kokic <3250679033@qq.com>
2023-03-13 08:28:45 +0800

    a first slash

--- fukugentex.mjs
+++ fukugentex.mjs
@@ -19,10 +19,19 @@ const braceWrap = x => lbrace.move(x).skip(rbrace)
 
 const macroName = letters.append(digit.asterisk()).plus()
 const macroh = backslash.move(macroName)
+const macrohc = backslash.append(macroName)
+
+const valueRef = () => value
+const unaryMacro = macrohc.append(macroName).append(valueRef)
+const value = braceWrap(valueRef).or(unaryMacro).or(macrohc).or(letters)
 
 const newcommand = macroh.check(x => x == "newcommand")
-  // .follow(value)
-  // .follow(value)
+  .move(value).follow(value)
+  .map(xs => lookupMap[xs[0]] = xs[1])
 
-console.log(newcommand.parse('\\newcommand'))
+const lookupMap = new Object()
+
+newcommand.parse('\\newcommand{\\CC}{\\mathbb}')
+
+console.log(lookupMap)
 
